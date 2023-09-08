f2c9d076803ed9369d166472a11561991960c44b
IamAbasz <ikponkeedet84@gmail.com>
2023-09-09 00:41:41 +0100

    update

--- script.js
+++ script.js
@@ -10,14 +10,17 @@ const daysOfTheWeek = [
 
 ];
 
+function updateTime(){
 
-const currentDayElement = document.getElementById('Current-day-Val');
+const currentDayElement = document.getElementById('Current-day-Value');
 const date = new Date ();
 const currentDayIndex = date.getDay();
 currentDayElement.innerText = daysOfTheWeek[currentDayIndex];
 
 // current time in milliseconds
 
-const currentTimeElement = document.getElementById('current-utc-time-val');
+const currentTimeElement = document.getElementById('current-utc-time-value');
 currentTimeElement.innerText = Date.now();
+}
+setInterval(updateTime, 1000);
 
